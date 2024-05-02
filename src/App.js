c6c3db097ff6dd1e78bdd319a8fc942d42db49d0
billywcollins <billy.collins86@gmail.com>
2024-05-01 20:51:01 -0500

    Revert "privacy policy route"
    This reverts commit 5d9ab9d2c16265b056a2136779b22de17a27c9b9.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './Pages/Home'
-import PrivacyPolicy from './Pages/PrivacyPolicy'
+import './App.css';
+import { useState } from 'react'
+import google from './google-play-badge.png'
+import apple from './app-store.svg'
+import character from './character.png'
+import Modal from './Components/Modal';
+import AppStoreButtons from './Components/AppStoreButtons';
 
 function App() {
 
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Home />,
-      errorElement: <div>404 Not Found</div>
-    },
-    {
-      path:'/privacy-policy',
-      element: <PrivacyPolicy />
-    }
-  ])
+  const [ isLive, setIsLive ] = useState(false)
+  const [ modalContent, setModalContent ] = useState('');
+  const [ showModal, setShowModal ] = useState(false);
+
+  function handleShowModal(content) {
+    setModalContent(content)
+    setShowModal(true)
+  }
+
+  function handleCloseModal() {
+    setShowModal(false)
+    setModalContent('')
+  }
 
   return (
-      <>
-        <RouterProvider router={router} />
-      </>
-    
+    <div className='container'>
+      <div className='hero'>
+        <h1>StorySprout</h1>
+        <p>Never-ending stories for kids and their big people.</p>
+      </div>
+
+      { isLive ? <AppStoreButtons buttons={[apple, google]}/> : <div className='coming-soon'><p>Coming Soon!</p></div> }
+
+      <div className="character">
+        <img src={character} alt='character' className="character-img"/>
+      </div>
+
+      <div className='footer'>
+        <p className="copyright">
+            © Copyright 2024 Very Good Apps LLC
+        </p>
+        <div className="footer-links">
+            <a onClick={() => handleShowModal({title: 'Privacy Policy', text: 'Some privacy Policy Text'})}>Privacy Policy</a>
+            <a onClick={() => handleShowModal({title: 'Support', text: 'Some Support content'})}>Support</a>
+        </div>
+        
+    </div>
+
+      {showModal && <Modal content={modalContent} cb={handleCloseModal}  />}
+
+    </div>
   );
 }
 
