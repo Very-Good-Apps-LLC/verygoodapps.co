459cebf3375cf245f3b84ca19ba14fca31274432
billywcollins <billy.collins86@gmail.com>
2024-06-22 05:19:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import Home from './Pages/Home'
 import PrivacyPolicy from './Pages/PrivacyPolicy'
 
 function App() {
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       path: '/',
       element: <Home />,
